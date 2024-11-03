7d2de55413e2f3aa98446a545cd6349983f582a5
Talha Baig <iamtalhabaig@gmail.com>
2024-11-03 10:35:58 +1000

    removed filament.scss

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-         'resources/css/filament.scss',
-                'resources/js/filament.js'
-      ],
+      
       refresh: true,
     }),
   ],
